daa73ef3c90c984b655ae60c0332f96a1ea027bb
mzz2017 <m@mzz.pub>
2019-01-05 03:15:13 +0800

    fix: 修复没有head的时候插入link失败的问题

--- shumsgHandlers/seoNormalizeHandler.go
+++ shumsgHandlers/seoNormalizeHandler.go
@@ -11,7 +11,7 @@ func SeoNormalizeHandler() func(*gin.Context, *http.Response, *string) *string {
 	return func(ctx *gin.Context, res *http.Response, body *string) *string {
 		r := *body
 		if strings.HasSuffix(res.Request.URL.Host, "shu.edu.cn") {
-			r = addSeoNormalize(*body)
+			r = addSeoNormalize(r)
 		}
 		return &r
 	}
@@ -20,8 +20,10 @@ func SeoNormalizeHandler() func(*gin.Context, *http.Response, *string) *string {
 /*添加seoNormalize.css*/
 func addSeoNormalize(s string) string {
 	r := regexp.MustCompile(`(?i)<head.*>`)
-	l := r.FindStringIndex(s)
-	LINK := `<link rel="stylesheet" type="text/css" href="https://shumsg.cn/static/seoNormalize.css">`
-	//将其加到<head>之后
-	return s[:l[1]] + LINK + s[l[1]:]
+	if l := r.FindStringIndex(s); len(l) > 0 {
+		LINK := `<link rel="stylesheet" type="text/css" href="https://shumsg.cn/static/seoNormalize.css">`
+		//将其加到<head>之后
+		return s[:l[1]] + LINK + s[l[1]:]
+	}
+	return s
 }
