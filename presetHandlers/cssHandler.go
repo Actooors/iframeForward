e1ee2b9a9fe6ee2d54505828bfdf6bcdf67dfdc2
mzz2017 <m@mzz.pub>
2019-01-05 19:35:34 +0800

    feat: 完善presetHandlers

--- presetHandlers/cssHandler.go
+++ presetHandlers/cssHandler.go
@@ -17,11 +17,32 @@ func CSSLinkHandler(src string) func(*gin.Context, *http.Response, *string) *str
 	}
 }
 
+func StyleHandler(styleContent string) func(*gin.Context, *http.Response, *string) *string {
+	return func(ctx *gin.Context, res *http.Response, body *string) *string {
+		r := *body
+		if strings.HasSuffix(res.Request.URL.Host, "shu.edu.cn") {
+			r = addStyle(r, styleContent)
+		}
+		return &r
+	}
+}
+
 /*添加<link>*/
 func addCSSLink(s string, src string) string {
 	r := regexp.MustCompile(`(?i)<head.*?>`)
 	if l := r.FindStringIndex(s); len(l) > 0 {
 		LINK := `<link rel="stylesheet" type="text/css" href="` + src + `">`
+		//将其加到<head>之后
+		return s[:l[1]] + LINK + s[l[1]:]
+	}
+	return s
+}
+
+/*添加<style></style>*/
+func addStyle(s string, styleContent string) string {
+	r := regexp.MustCompile(`(?i)<head.*?>`)
+	if l := r.FindStringIndex(s); len(l) > 0 {
+		LINK := `<style>` + styleContent + `</style>`
 		//将其加到<head>之后
 		return s[:l[1]] + LINK + s[l[1]:]
 	}
--- presetHandlers/scriptHandler.go
+++ presetHandlers/scriptHandler.go
@@ -13,11 +13,30 @@ func ScriptHandler(scriptContent string) func(*gin.Context, *http.Response, *str
 	}
 }
 
+//https://cdn.bootcss.com/inobounce/0.1.6/inobounce.min.js
+func ScriptFromHandler(src string) func(*gin.Context, *http.Response, *string) *string {
+	return func(ctx *gin.Context, res *http.Response, body *string) *string {
+		r := addScriptFrom(*body, src)
+		return &r
+	}
+}
+
 /*添加<script></script>*/
 func addScript(s string, scriptContent string) string {
 	r := regexp.MustCompile(`(?i)<head.*?>`)
 	if l := r.FindStringIndex(s); len(l) > 0 {
 		LINK := `<script>` + scriptContent + `</script>`
+		//将其加到<head>之后
+		return s[:l[1]] + LINK + s[l[1]:]
+	}
+	return s
+}
+
+/*添加<script src="..."></script>*/
+func addScriptFrom(s string, src string) string {
+	r := regexp.MustCompile(`(?i)<head.*?>`)
+	if l := r.FindStringIndex(s); len(l) > 0 {
+		LINK := `<script src="` + src + `"></script>`
 		//将其加到<head>之后
 		return s[:l[1]] + LINK + s[l[1]:]
 	}
